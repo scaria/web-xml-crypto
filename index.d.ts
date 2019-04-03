2025f162c2550e306288b9096c14df07dffdc2a6
Rex Scaria <arunrex@cloudflare.com>
2019-04-03 11:15:20 -0500

    V1.2.3

--- index.d.ts
+++ index.d.ts
@@ -4,12 +4,48 @@ declare module "web-xml-crypto" {
     getKey: (...args: any[]) => string;
     getKeyInfo: (...args: any[]) => string;
   }
+
+  type SignatureAlgorithm = {
+    getSignature(info: string, key: Buffer | Uint8Array): string;
+    verifySignature(
+      info: string,
+      key: Buffer | Uint8Array,
+      signature: string
+    ): boolean;
+  };
+
+  type CanonizationAlgorithm = {};
+  type HashAlgorithm = {
+    getHash(string: string): string;
+    getAlgorithmName(): string;
+  };
+
   export class SignedXml {
+    signingKey: Buffer | Uint8Array;
     signatureAlgorithm: any;
     keyInfoProvider: FileKeyInfo;
+    addReference(ref: string): void;
     loadSignature: (node: any) => void;
     checkSignature: (xml: string, xmlDoc?: Document) => boolean;
-    computeSignature: (xml: string) => string;
+    computeSignature: (
+      xml: string,
+      oprts?: { prefix?: string; location?: any; attrs?: any }
+    ) => void;
+    getSignedXml(): string;
+    validateSignatureValue(doc: Document): boolean;
+    findSignatureAlgorithm(name: string): SignatureAlgorithm;
+    findCanonicalizationAlgorithm(name: string): CanonizationAlgorithm;
+    findHashAlgorithm(name: string): HashAlgorithm;
+    validateReferences(doc: Document): boolean;
+    loadReference(xpath: string): void;
+    getKeyInfo(prefix: string): string;
+    createReferences(doc: Document, prefix: string): string;
+    getSignatureXml(): string;
+    getOriginalXmlWithIds(): string;
+    getSignedXml(): string;
+    createSignature(info: string, prefix: string): string;
+    createSignedInfo(doc: Document, prefix: string): string;
+    ensureHasId(node: Node | Document): string;
     references: any[];
   }
 }
